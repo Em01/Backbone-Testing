3357fca0ddd99d2c3368a2bddbc3457c2e0591cf
Em01 <emma_williams1@hotmail.co.uk>
2015-06-12 12:21:29 +0100

    Initial test for view

---
+++ views/Testing Views/spec/TweetViewSpec.js
@@ -0,0 +1,15 @@
+
+describe("TweetView", function(){
+	var view;
+	var model;
+
+	beforeEach(function(){
+		model = new Tweet();
+		view = new TweetView({ model: model });
+		view.render();
+	});
+
+	it("tagName should be li.", function(){
+		expect(view.tagName).toEqual("li");
+	});
+});
